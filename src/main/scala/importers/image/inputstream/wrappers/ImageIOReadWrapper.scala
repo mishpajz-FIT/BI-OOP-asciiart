09eb7661318668aa6046d1de27bf177101757c7b
Michal Dobeš <dobesmic@fit.cvut.cz>
2023-12-10 18:37:00 +0100

    docs: add ScalaDocs for Wrappers

--- src/main/scala/importers/image/inputstream/wrappers/ImageIOReadWrapper.scala
+++ src/main/scala/importers/image/inputstream/wrappers/ImageIOReadWrapper.scala
@@ -4,7 +4,17 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import javax.imageio.stream.ImageInputStream
 
+/**
+  * Wrapper for [[ImageIO]] read method.
+  */
 trait ImageIOReadWrapper {
+
+  /**
+    * Reads [[BufferedImage]] from [[ImageInputStream]].
+    * 
+    * @param inputStream [[ImageInputStream]] to read from
+    * @return [[BufferedImage]]
+    */
   def ioRead(inputStream: ImageInputStream): BufferedImage =
     ImageIO.read(inputStream)
 }
